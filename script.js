afa8ff81b74e40829c5f213a3843e9ff4d92b6e8
Daksh Pratap Singh <73421193+Daksh4568@users.noreply.github.com>
2024-02-26 17:12:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "91930a5025b94edf90e1898516c2fa12";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
